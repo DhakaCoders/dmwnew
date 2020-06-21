eb2e22c0eac3040b4ab51970c0689831d3f75f9d
Rannojit <cserannojitsarkar22@gmail.com>
2020-06-22 05:56:10 +0600

    responsive done

--- themes/dmwatch/new-page-tools.php
+++ themes/dmwatch/new-page-tools.php
@@ -152,12 +152,13 @@ get_header();
         <div class="dm-tp-report-sec-inr">
           <h1 class="dm-tp-report-title">Lorem Ipsum Adaptation Reports</h1>
           <div class="dm-tp-report-des clearfix">
-            <div class="dm-tp-report-des-left">
-              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-            </div>
             <div class="dm-tp-report-des-img">
               <img src="<?php echo THEME_URI; ?>/assets/images/dm-tp-bd-map.png">
             </div>
+            <div class="dm-tp-report-des-left">
+              <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+            </div>
+            
           </div>
         </div>
       </div>
@@ -209,7 +210,7 @@ get_header();
       <div class="col-md-12">
         <div class="dm-tp-des-sec-inr">
           <h1 class="dm-tp-des-title">Lorem ipsum</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br> industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and <br> scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into <br> electronic typesetting, remaining essentially unchanged.</p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
         </div>
       </div>
     </div>
